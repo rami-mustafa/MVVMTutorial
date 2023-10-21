1a4af2b40ebe5c1b576a8d6ec213d649c84da215
Rami Mustafa <ramimustafa77@gmail.com>
2023-10-21 22:27:26 +0200

    Main View Model

---
+++ MVVMPattern/MVVMPattern/ViewModel/MainViewModel.swift
@@ -0,0 +1,17 @@
+
+import Foundation
+
+
+class MainViewModel {
+    
+    
+    func numberOfSections() -> Int {
+        1
+    }
+    
+    func numberOfRows(in section: Int) -> Int {
+      10
+    }
+    
+    
+}
